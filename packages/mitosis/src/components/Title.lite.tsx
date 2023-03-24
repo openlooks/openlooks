8907c193485c81801ea856337c91744050f279c4
Cody Ebberson <cody@ebberson.com>
2023-03-24 13:36:39 -0700

    Home page fixes

--- packages/mitosis/src/components/Title.lite.tsx
+++ packages/mitosis/src/components/Title.lite.tsx
@@ -1,8 +1,10 @@
 import { Show } from '@builder.io/mitosis';
+import { JSX } from '@builder.io/mitosis/jsx-runtime';
+import { buildOpenLooksClassName } from '../utils/classname';
+import { BaseComponentProps } from './BaseComponentProps';
 
-export interface TitleProps {
+export interface TitleProps extends BaseComponentProps {
   order: 1 | 2 | 3 | 4 | 5 | 6;
-  children?: any;
 }
 
 export default function Title(props: TitleProps) {
@@ -10,22 +12,34 @@ export default function Title(props: TitleProps) {
   return (
     <>
       <Show when={props.order === 1}>
-        <h1 class="openlooks title">{props.children}</h1>
+        <h1 class={buildOpenLooksClassName('title', props)} style={props.sx as JSX.CSS | undefined}>
+          {props.children}
+        </h1>
       </Show>
       <Show when={props.order === 2}>
-        <h2 class="openlooks title">{props.children}</h2>
+        <h2 class={buildOpenLooksClassName('title', props)} style={props.sx as JSX.CSS | undefined}>
+          {props.children}
+        </h2>
       </Show>
       <Show when={props.order === 3}>
-        <h3 class="openlooks title">{props.children}</h3>
+        <h3 class={buildOpenLooksClassName('title', props)} style={props.sx as JSX.CSS | undefined}>
+          {props.children}
+        </h3>
       </Show>
       <Show when={props.order === 4}>
-        <h4 class="openlooks title">{props.children}</h4>
+        <h4 class={buildOpenLooksClassName('title', props)} style={props.sx as JSX.CSS | undefined}>
+          {props.children}
+        </h4>
       </Show>
       <Show when={props.order === 5}>
-        <h5 class="openlooks title">{props.children}</h5>
+        <h5 class={buildOpenLooksClassName('title', props)} style={props.sx as JSX.CSS | undefined}>
+          {props.children}
+        </h5>
       </Show>
       <Show when={props.order === 6}>
-        <h6 class="openlooks title">{props.children}</h6>
+        <h6 class={buildOpenLooksClassName('title', props)} style={props.sx as JSX.CSS | undefined}>
+          {props.children}
+        </h6>
       </Show>
     </>
   );
--- packages/mitosis/src/site/pages/HomePage.lite.tsx
+++ packages/mitosis/src/site/pages/HomePage.lite.tsx
@@ -3,29 +3,54 @@ import Container from '../../components/Container.lite';
 import Markdown from '../../components/Markdown.lite';
 import Title from '../../components/Title.lite';
 import Counter from '../components/Counter.lite';
+import DocHeader from '../components/DocHeader.lite';
 import Prism from '../components/Prism.lite';
 
 export default function HomePage() {
   return (
-    <Container size="sm">
-      <Title order={1}>OpenLooks</Title>
-      <div>
-        <Anchor href="https://openlooks.dev">OpenLooks</Anchor>
-      </div>
-      <hr />
-      <Title order={2}>Counter Test</Title>
-      <Counter />
-      <hr />
-      <Title order={2}>Markdown Test</Title>
-      <Markdown>{`### Hello World
+    <>
+      <DocHeader
+        title="OpenLooks"
+        description="Cross-framework components and utilities for building modern web applications."
+      />
+      <Container size="sm">
+        <Title order={2}>What is this?</Title>
+        <ul class="openlooks text">
+          <li>
+            Component library based on the look and feel of{` `}
+            <Anchor href="https://mantine.dev">Mantine</Anchor>
+          </li>
+          <li>
+            Uses{` `}
+            <Anchor href="https://react.dev">Mitosis</Anchor>
+            {` `}to support popular frameworks such as{` `}
+            <Anchor href="https://react.dev">React</Anchor>,{` `}
+            <Anchor href="https://react.dev">Angular</Anchor>,{` `}
+            <Anchor href="https://react.dev">Vue</Anchor>,{` `}
+            <Anchor href="https://react.dev">Svelte</Anchor>,{` `}
+            <Anchor href="https://react.dev">Solid</Anchor>,{` `}and more.
+          </li>
+          <li>Zero dependencies and minimal footprint -- this whole site is only 20kb!</li>
+          <li>100% TypeScript</li>
+          <li>MIT license</li>
+        </ul>
+        <Title order={2} mt="xl">
+          Counter Test
+        </Title>
+        <Counter />
+        <Title order={2} mt="xl">
+          Markdown Test
+        </Title>
+        <Markdown>{`### Hello World
 
       This is a Markdown test.
 
-      *Foo* **bar** _bang_ bop.
+      *Foo* **bar** ***baz***.
       `}</Markdown>
-      <hr />
-      <Title order={2}>Code Block</Title>
-      <Prism language="jsx">{`import { Button } from '@mantine/core';
+        <Title order={2} mt="xl">
+          Code Block
+        </Title>
+        <Prism language="jsx">{`import { Button } from '@mantine/core';
 
 function Demo() {
   return (
@@ -34,6 +59,7 @@ function Demo() {
     </Button>
   );
 }`}</Prism>
-    </Container>
+      </Container>
+    </>
   );
 }
