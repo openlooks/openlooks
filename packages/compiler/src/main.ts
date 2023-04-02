49c05baeb869d6fa6e940235f5871b214b4db8d7
Cody Ebberson <cody@ebberson.com>
2023-04-01 17:08:40 -0700

    Solid compiler

--- packages/compiler/src/main.ts
+++ packages/compiler/src/main.ts
@@ -2,6 +2,7 @@ import fastGlob from 'fast-glob';
 import { resolve } from 'path';
 import ts from 'typescript';
 import { transformToReact } from './react';
+import { transformToSolid } from './solid';
 
 async function main(): Promise<void> {
   const inputFiles = await fastGlob(['../mitosis/src/**/*', '../mitosis/public/**/*']);
@@ -12,34 +13,8 @@ async function main(): Promise<void> {
   );
 
   transformToReact(program, inputFiles, '../react');
+  transformToSolid(program, inputFiles, '../solid');
 }
-
-// function buildReact(program: ts.Program, inputFiles: string[], outDir: string): void {
-//   for (const inputFile of inputFiles) {
-//     if (!inputFile.endsWith('.ts') && !inputFile.endsWith('.tsx')) {
-//       const targetFileName = resolve(outDir, inputFile.replace('../mitosis/', ''));
-//       ensureDirectoryExists(targetFileName);
-//       fs.copyFileSync(inputFile, targetFileName);
-//       continue;
-//     }
-
-//     const source = program.getSourceFile(inputFile) as ts.SourceFile;
-//     const result = ts.transform(source, [transformer(program)]);
-//     const printer = ts.createPrinter();
-
-//     for (const output of result.transformed) {
-//       const targetFileName = resolve(
-//         outDir,
-//         inputFile.replace('../mitosis/', '').replace('.lite.ts', '.ts').replace('.lite.tsx', '.tsx')
-//       );
-
-//       const transformerOutput = printer.printFile(output);
-//       const prettierOutput = prettier.format(transformerOutput, { filepath: targetFileName });
-//       ensureDirectoryExists(targetFileName);
-//       fs.writeFileSync(targetFileName, prettierOutput, 'utf8');
-//     }
-//   }
-// }
 
 main()
   .then(() => console.log('done'))
---
+++ packages/compiler/src/solid.ts
@@ -0,0 +1,197 @@
+import fs from 'fs';
+import { resolve } from 'path';
+import prettier from 'prettier';
+import ts from 'typescript';
+import {
+  ensureDirectoryExists,
+  getSetterName,
+  isFunctionCall,
+  isJsxElement,
+  isLiteImport,
+  isLiteReexport,
+  isMitosisImport,
+  isStateRead,
+  isStateWrite,
+  isUseStoreDeclaration,
+  tryGetFullText,
+} from './utils';
+
+export function transformToSolid(program: ts.Program, inputFiles: string[], outDir: string): void {
+  for (const inputFile of inputFiles) {
+    if (!inputFile.endsWith('.ts') && !inputFile.endsWith('.tsx')) {
+      const targetFileName = resolve(outDir, inputFile.replace('../mitosis/', ''));
+      ensureDirectoryExists(targetFileName);
+      fs.copyFileSync(inputFile, targetFileName);
+      continue;
+    }
+
+    const source = program.getSourceFile(inputFile) as ts.SourceFile;
+    const result = ts.transform(source, [transformer(program)]);
+    const printer = ts.createPrinter();
+
+    for (const output of result.transformed) {
+      const targetFileName = resolve(
+        outDir,
+        inputFile.replace('../mitosis/', '').replace('.lite.ts', '.ts').replace('.lite.tsx', '.tsx')
+      );
+
+      const transformerOutput = printer.printFile(output);
+      const prettierOutput = prettier.format(transformerOutput, { filepath: targetFileName });
+      ensureDirectoryExists(targetFileName);
+      fs.writeFileSync(targetFileName, prettierOutput, 'utf8');
+    }
+  }
+}
+
+function transformer(program: ts.Program): ts.TransformerFactory<ts.SourceFile> {
+  if (!program) {
+    throw new Error('Missing program');
+  }
+
+  const checker = program.getTypeChecker();
+  if (!checker) {
+    throw new Error('Missing checker');
+  }
+
+  const solidImports = new Set<string>();
+
+  return (context: ts.TransformationContext) => {
+    const visitor: ts.Visitor = (node) => {
+      node = ts.visitEachChild(node, visitor, context);
+
+      // Remove mitosis imports
+      if (isMitosisImport(node)) {
+        return undefined;
+      }
+
+      // Remove ".lite" suffixes from import file names
+      if (isLiteImport(node)) {
+        return ts.factory.updateImportDeclaration(
+          node,
+          node.modifiers,
+          node.importClause,
+          ts.factory.createStringLiteral(node.moduleSpecifier.text.replace('.lite', '')),
+          undefined
+        );
+      }
+
+      // Remove ".lite" suffixes from re-export file names
+      if (isLiteReexport(node)) {
+        return ts.factory.updateExportDeclaration(
+          node,
+          node.modifiers,
+          node.isTypeOnly,
+          node.exportClause,
+          ts.factory.createStringLiteral(node.moduleSpecifier.text.replace('.lite', '')),
+          undefined
+        );
+      }
+
+      if (ts.isTypeNode(node)) {
+        const typeNode = node as ts.TypeNode;
+        const typeStr = tryGetFullText(typeNode);
+
+        // React: Rewrite "JSX.CSS" to "JSX.CSSProperties"
+        if (typeStr === 'JSX.CSS') {
+          solidImports.add('JSX');
+          return ts.factory.createTypeReferenceNode('JSX.CSSProperties');
+        }
+      }
+
+      // React: Rewrite "useStore" to "createSignal"
+      if (isUseStoreDeclaration(node)) {
+        solidImports.add('createSignal');
+        const useStateStmts: ts.Statement[] = [];
+        for (const prop of node.declarationList.declarations[0].initializer.arguments[0].properties) {
+          const propAssignment = prop as ts.PropertyAssignment;
+          const propName = (propAssignment.name as ts.Identifier).text;
+          useStateStmts.push(
+            ts.factory.createVariableStatement(
+              undefined,
+              ts.factory.createVariableDeclarationList(
+                [
+                  ts.factory.createVariableDeclaration(
+                    ts.factory.createArrayBindingPattern([
+                      ts.factory.createBindingElement(undefined, undefined, propName, undefined),
+                      ts.factory.createBindingElement(undefined, undefined, getSetterName(propName), undefined),
+                    ]),
+                    undefined,
+                    undefined,
+                    ts.factory.createCallExpression(ts.factory.createIdentifier('createSignal'), undefined, [
+                      propAssignment.initializer,
+                    ])
+                  ),
+                ],
+                ts.NodeFlags.Const
+              )
+            )
+          );
+        }
+        return useStateStmts;
+      }
+
+      // React: Replace state read with state getter
+      if (isStateRead(node)) {
+        return ts.factory.createCallExpression(ts.factory.createIdentifier(node.name.text), undefined, []);
+      }
+
+      // React: Replace state write with state setter
+      if (isStateWrite(node)) {
+        return ts.factory.createCallExpression(
+          ts.factory.createIdentifier(getSetterName(node.left.name.text)),
+          undefined,
+          [node.right]
+        );
+      }
+
+      if (isFunctionCall(node, 'createContext')) {
+        solidImports.add('createContext');
+      }
+
+      if (isFunctionCall(node, 'useContext')) {
+        solidImports.add('useContext');
+      }
+
+      if (isFunctionCall(node, 'onMount')) {
+        solidImports.add('onMount');
+      }
+
+      if (isJsxElement(node, 'For')) {
+        solidImports.add('For');
+      }
+
+      if (isJsxElement(node, 'Show')) {
+        solidImports.add('Show');
+      }
+
+      return node;
+    };
+
+    // Return transformer factory
+    return (sourceFile: ts.SourceFile) => {
+      // Visit all nodes
+      const outputSourceFile = ts.visitNode(sourceFile, visitor) as ts.SourceFile;
+
+      if (solidImports.size > 0) {
+        return ts.factory.updateSourceFile(outputSourceFile, [
+          ts.factory.createImportDeclaration(
+            undefined,
+            ts.factory.createImportClause(
+              false,
+              undefined,
+              ts.factory.createNamedImports(
+                Array.from(solidImports)
+                  .sort()
+                  .map((i) => ts.factory.createImportSpecifier(false, undefined, ts.factory.createIdentifier(i)))
+              )
+            ),
+            ts.factory.createStringLiteral('solid-js')
+          ),
+          ...outputSourceFile.statements,
+        ]);
+      } else {
+        return outputSourceFile;
+      }
+    };
+  };
+}
