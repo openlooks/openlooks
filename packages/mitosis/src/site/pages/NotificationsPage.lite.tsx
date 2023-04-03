ef857ea4a3af318f048b655d096b9f39bfd2e705
Cody Ebberson <cody@ebberson.com>
2023-04-03 12:59:34 -0700

    Fixed mobile styles on notifications demo

--- packages/mitosis/src/site/pages/NotificationsPage.lite.tsx
+++ packages/mitosis/src/site/pages/NotificationsPage.lite.tsx
@@ -1,109 +1,166 @@
 import Button from '../../components/Button.lite';
+import Center from '../../components/Center.lite';
 import Group from '../../components/Group.lite';
 import { showNotification } from '../../components/NotificationsManager';
+import Paper from '../../components/Paper.lite';
+import Text from '../../components/Text.lite';
 import Title from '../../components/Title.lite';
 import DocPage from '../components/DocPage.lite';
+import Prism from '../components/Prism.lite';
 
 export default function NotificationsPage() {
   return (
     <DocPage title="Notifications" description="Notification system">
+      <Title order={2}>Demo</Title>
+      <Paper p="xl" mb="xl" radius="md" withBorder>
+        <Group
+          sx={{
+            'flex-flow': 'row wrap',
+            'align-items': 'center',
+            'justify-content': 'center',
+            gap: '16px',
+          }}
+        >
+          <Button
+            color="blue"
+            variant="outline"
+            onClick={() =>
+              showNotification({
+                color: 'blue',
+                title: 'Default notification',
+                message: 'Hey there, your code is awesome! 🤥',
+              })
+            }
+          >
+            Default
+          </Button>
+          <Button
+            color="teal"
+            variant="outline"
+            onClick={() =>
+              showNotification({
+                color: 'teal',
+                title: 'You did great',
+                message: 'Data was saved',
+              })
+            }
+          >
+            Teal with icon
+          </Button>
+          <Button
+            color="red"
+            variant="outline"
+            onClick={() =>
+              showNotification({
+                color: 'red',
+                title: 'Bummer!',
+                message: 'You have no right to do this',
+              })
+            }
+          >
+            Red color
+          </Button>
+          <Button
+            color="grape"
+            variant="outline"
+            onClick={() =>
+              showNotification({
+                color: 'grape',
+                title: 'I will never close',
+                message: 'unless you click X',
+                autoClose: false,
+              })
+            }
+          >
+            Never closes automatically
+          </Button>
+          <Button
+            color="indigo"
+            variant="outline"
+            onClick={() =>
+              showNotification({
+                color: 'indigo',
+                title: 'Custom autoClose timeout',
+                message: 'Notification will be closed in 10 seconds',
+              })
+            }
+          >
+            10 seconds timeout
+          </Button>
+          <Button
+            color="cyan"
+            variant="outline"
+            onClick={() =>
+              showNotification({
+                color: 'cyan',
+                title: 'Loading your data',
+                message: 'Data will be loaded in 3 seconds, you cannot close this yet',
+                disallowClose: true,
+                onClose: () =>
+                  showNotification({
+                    color: 'teal',
+                    title: 'Data was loaded',
+                    message: 'Notification will close in 3 seconds, you can close this notification now',
+                  }),
+              })
+            }
+          >
+            Loading state and update
+          </Button>
+        </Group>
+      </Paper>
       <Title order={2}>Usage</Title>
-      <Group
-        sx={{
-          width: '600px',
-          'flex-flow': 'row wrap',
-          'align-items': 'center',
-          'justify-content': 'center',
-          gap: '16px',
-        }}
+      <Text>
+        Add Notifications component anywhere in your application, usually it is placed inside MantineProvider next to
+        your App component:
+      </Text>
+      <Prism language="jsx">{`import { Notifications } from '@openlooks/react';
+
+function Demo() {
+  return (
+    <>
+      <Notifications />
+      <App />
+    </>
+  );
+}`}</Prism>
+      <Text my="xl">Then use notifications.show function anywhere in your application:</Text>
+      <Paper p="xl" mb="xl" radius="md" withBorder>
+        <Center>
+          <Button
+            color="blue"
+            variant="outline"
+            onClick={() =>
+              showNotification({
+                color: 'blue',
+                title: 'Default notification',
+                message: 'Hey there, your code is awesome! 🤥',
+              })
+            }
+          >
+            Show notification
+          </Button>
+        </Center>
+      </Paper>
+      <Prism language="jsx">{`import { Group, Button, showNotification } from '@openlooks/react';
+
+function Demo() {
+  return (
+    <Group position="center">
+      <Button
+        variant="outline"
+        onClick={() =>
+          showNotification({
+            title: 'Default notification',
+            message: 'Hey there, your code is awesome! 🤥',
+          })
+        }
       >
-        <Button
-          color="blue"
-          variant="outline"
-          onClick={() =>
-            showNotification({
-              color: 'blue',
-              title: 'Default notification',
-              message: 'Hey there, your code is awesome! 🤥',
-            })
-          }
-        >
-          Default
-        </Button>
-        <Button
-          color="teal"
-          variant="outline"
-          onClick={() =>
-            showNotification({
-              color: 'teal',
-              title: 'You did great',
-              message: 'Data was saved',
-            })
-          }
-        >
-          Teal with icon
-        </Button>
-        <Button
-          color="red"
-          variant="outline"
-          onClick={() =>
-            showNotification({
-              color: 'red',
-              title: 'Bummer!',
-              message: 'You have no right to do this',
-            })
-          }
-        >
-          Red color
-        </Button>
-        <Button
-          color="grape"
-          variant="outline"
-          onClick={() =>
-            showNotification({
-              color: 'grape',
-              title: 'I will never close',
-              message: 'unless you click X',
-              autoClose: false,
-            })
-          }
-        >
-          Never closes automatically
-        </Button>
-        <Button
-          color="indigo"
-          variant="outline"
-          onClick={() =>
-            showNotification({
-              color: 'indigo',
-              title: 'Custom autoClose timeout',
-              message: 'Notification will be closed in 10 seconds',
-            })
-          }
-        >
-          10 seconds timeout
-        </Button>
-        <Button
-          color="cyan"
-          variant="outline"
-          onClick={() =>
-            showNotification({
-              color: 'cyan',
-              title: 'Loading your data',
-              message: 'Data will be loaded in 3 seconds, you cannot close this yet',
-              disallowClose: true,
-              onClose: () =>
-                showNotification({
-                  color: 'teal',
-                  title: 'Data was loaded',
-                  message: 'Notification will close in 3 seconds, you can close this notification now',
-                }),
-            })
-          }
-        >
-          Loading state and update
-        </Button>
-      </Group>
+        Show notification
+      </Button>
+    </Group>
+  );
+}`}</Prism>
     </DocPage>
   );
 }
