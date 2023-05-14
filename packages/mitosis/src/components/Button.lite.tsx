ef6f2677b8284bfc216059c993fb5ec29e65eb98
Cody Ebberson <cody@ebberson.com>
2023-05-14 10:17:32 -0700

    Button icons and loading state

--- packages/mitosis/src/components/Button.lite.tsx
+++ packages/mitosis/src/components/Button.lite.tsx
@@ -1,10 +1,14 @@
+import { Show } from '@builder.io/mitosis';
 import { JSX } from '@builder.io/mitosis/jsx-runtime';
 import { buildOpenLooksClassName } from '../utils/classname';
+import Loader from './Loader.lite';
 
 export interface ButtonProps {
   id?: string;
   c?: string;
   sx?: Record<string, any>;
+  slotIcon?: JSX.Element;
+  loading?: boolean;
   onClick?: (e: any) => void;
   children?: any;
 }
@@ -16,7 +20,17 @@ export default function Button(props: ButtonProps) {
       class={buildOpenLooksClassName('button', props.c, { variant: 'filled', color: 'blue', size: 'sm', radius: 'sm' })}
       style={props.sx as JSX.CSS | undefined}
       onClick={(event) => props.onClick?.(event)}
+      data-loading={props.loading}
+      disabled={props.loading}
     >
+      <Show when={props.slotIcon && !props.loading}>
+        <div class={buildOpenLooksClassName('icon', undefined, { variant: 'none' })}>{props.slotIcon}</div>
+      </Show>
+      <Show when={props.loading}>
+        <div class={buildOpenLooksClassName('icon', undefined, { variant: 'none' })}>
+          <Loader c="color-white variant-none" />
+        </div>
+      </Show>
       {props.children}
     </button>
   );
--- packages/mitosis/src/site/pages/ButtonPage.lite.tsx
+++ packages/mitosis/src/site/pages/ButtonPage.lite.tsx
@@ -6,9 +6,13 @@ import Configurator from '../../components/Configurator.lite';
 import ConfiguratorControls from '../../components/ConfiguratorControls.lite';
 import ConfiguratorStage from '../../components/ConfiguratorStage.lite';
 import NativeSelect from '../../components/NativeSelect.lite';
+import RouterLink from '../../components/RouterLink.lite';
 import Stack from '../../components/Stack.lite';
+import Switch from '../../components/Switch.lite';
+import Text from '../../components/Text.lite';
 import TextInput from '../../components/TextInput.lite';
 import Title from '../../components/Title.lite';
+import IconDatabase from '../../icons/IconDatabase.lite';
 import DocPage from '../components/DocPage.lite';
 import Prism from '../components/Prism.lite';
 import SizeInput from '../components/SizeInput.lite';
@@ -20,6 +24,7 @@ export default function ButtonPage() {
     radius: 'sm' as Size,
     size: 'sm' as Size,
     text: 'Settings',
+    loading: false,
   });
 
   return (
@@ -86,6 +91,43 @@ function Demo() {
   return (
     <Button c="variant-${state.variant} color-${state.color} radius-${state.radius} size-${state.size}">
       ${state.text}
+    </Button>
+  );
+}`}
+      />
+      <Title order={2} c="mt-xl">
+        Loading state
+      </Title>
+      <Text>
+        Button supports loading state. In this state <RouterLink href="/loader">Loader</RouterLink> component replaces
+        left or right icon, button becomes disabled and white or dark overlay is added.
+      </Text>
+      <Configurator>
+        <ConfiguratorStage>
+          <Button slotIcon={<IconDatabase />} loading={state.loading}>
+            Connect to database
+          </Button>
+        </ConfiguratorStage>
+        <ConfiguratorControls>
+          <Stack>
+            <Switch
+              id="loading"
+              label="Loading"
+              c="radius-xl"
+              onChange={(event) => {
+                state.loading = (event.target as HTMLInputElement).checked;
+              }}
+            />
+          </Stack>
+        </ConfiguratorControls>
+      </Configurator>
+      <Prism
+        language="jsx"
+        code={`import { Button } from '@openlooks/react';
+function Demo() {
+  return (
+    <Button slotIcon={<IconDatabase />} loading={${state.loading}}>
+      Connect to database
     </Button>
   );
 }`}
